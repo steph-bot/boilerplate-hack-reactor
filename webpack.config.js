ce6979872766c710b45cdcd59d8c38ce2e0214eb
Tom Chandler <chandlertom71@gmail.com>
2019-11-27 15:32:28 -0600

    setting up basic file structure

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/dist');
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
